238ab7e2c64b2917480a6e06648b4fe830d99afc
bugweaver <illustrati0@yandex.ru>
2025-02-26 09:00:46 +0300

    Add hash_password function

--- src/auth/utils.py
+++ src/auth/utils.py
@@ -27,3 +27,12 @@ def decode_jwt(
         algorithms=[algorithm],
     )
     return decoded
+
+
+def hash_password(
+        password: str,
+) -> bytes:
+    salt = bcrypt.gensalt()
+    pwd_bytes: bytes = password.encode()
+    return bcrypt.hashpw(pwd_bytes, salt)
+
