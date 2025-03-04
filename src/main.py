aa109a6903704ccbec5cd64c4fe29b7890a23af3
bugweaver <illustrati0@yandex.ru>
2025-03-04 14:03:44 +0300

    Add redis client to the lifespan

--- src/main.py
+++ src/main.py
@@ -1,20 +1,28 @@
 from contextlib import asynccontextmanager
+from typing import AsyncIterator
 
 import uvicorn
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 
+from api import router as api_router
+from core.auth.services.redis_service import setup_redis_client
 from core.config import settings
 
 from api import router as api_router
 from core.models import db_helper
 
+
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def lifespan(app: FastAPI) -> AsyncIterator[None]:
     # startapp
+    app.state.redis_client = await setup_redis_client()
     yield
     # shutdown
     await db_helper.dispose()
+    await app.state.redis_client.close()
+
+
 app = FastAPI(
     default_response_class=ORJSONResponse,
     lifespan=lifespan,
