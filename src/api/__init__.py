6d4bf268d5e3f607b3585d4e9d62f14b9566c462
bugweaver <illustrati0@yandex.ru>
2025-03-04 14:34:10 +0300

    Configure imports

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -2,6 +2,9 @@ from fastapi import APIRouter
 
 from core.config import settings
 
+from .v1 import router as api_v1
+
 router = APIRouter(
     prefix=settings.api.prefix,
 )
+router.include_router(api_v1)
