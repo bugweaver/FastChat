cd370f29f4a32a9739eebfe4a6d9b2944c3ad142
bugweaver <illustrati0@yandex.ru>
2025-04-14 10:40:12 +0300

    refactor: add chat and websocket routers to init file

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -3,6 +3,8 @@ from fastapi import APIRouter
 from core.config import settings
 
 from .auth_router import router as auth_router
+from .chat_router import router as chat_router
+from .websocket_router import router as websocket_router
 
 router = APIRouter(
     prefix=settings.api.v1.prefix,
@@ -10,4 +12,12 @@ router = APIRouter(
 router.include_router(
     auth_router,
     prefix=settings.api.v1.auth,
+)
+router.include_router(
+    websocket_router,
+    prefix=settings.api.v1.ws,
+)
+router.include_router(
+    chat_router,
+    prefix=settings.api.v1.chat,
 )
