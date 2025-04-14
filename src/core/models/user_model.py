24116635a368f8c9a82d8c146566a2f527bc28ec
bugweaver <illustrati0@yandex.ru>
2025-04-14 09:06:41 +0300

    refactor(user_model): update User model with avatar, last_seen, and relationships

--- src/core/models/user_model.py
+++ src/core/models/user_model.py
@@ -1,10 +1,15 @@
 from datetime import datetime
+from typing import TYPE_CHECKING, List
 
 from sqlalchemy import DateTime, func
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from core.models import Base
-from core.models.mixins import IntIdPkMixin
+from .base import Base
+from .mixins.int_id_pk import IntIdPkMixin
+
+if TYPE_CHECKING:
+    from .chat_part_model import ChatParticipant
+    from .message_model import Message
 
 
 class User(IntIdPkMixin, Base):
@@ -13,7 +18,12 @@ class User(IntIdPkMixin, Base):
     last_name: Mapped[str] = mapped_column(nullable=True)
     username: Mapped[str] = mapped_column(unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
+    avatar: Mapped[str | None] = mapped_column(nullable=True)
     is_active: Mapped[bool] = mapped_column(default=True)
+    last_seen: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+    )
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
         server_default=func.now(),
@@ -21,6 +31,10 @@ class User(IntIdPkMixin, Base):
     updated_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True),
         server_default=func.now(),
-        server_onupdate=func.now(),
         onupdate=func.now(),
     )
+
+    messages_sent: Mapped[List["Message"]] = relationship(
+        back_populates="sender", foreign_keys="Message.sender_id"
+    )
+    chats: Mapped[List["ChatParticipant"]] = relationship(back_populates="user")
