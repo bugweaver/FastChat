b0591479e80c7fbbfde59c44bac1f1e4ac81af17
bugweaver <illustrati0@yandex.ru>
2025-04-14 10:51:38 +0300

    tests(factories): create user factory

---
+++ src/tests/factories/user_factory.py
@@ -0,0 +1,44 @@
+from typing import Any
+
+import factory
+
+from core.auth.utils.password_utils import hash_password
+from core.models import User
+from core.schemas.user_schemas import UserRegister
+
+from .base import AsyncSQLAlchemyModelFactory
+
+
+class UserFactory(AsyncSQLAlchemyModelFactory):
+    """Factory for creating test users."""
+
+    class Meta:
+        model = User
+        sqlalchemy_session = None
+        sqlalchemy_session_persistence = None
+
+    email = factory.Faker("email")
+    username = factory.Faker("user_name")
+    password = factory.LazyAttribute(
+        lambda o: hash_password("testpassword").decode("utf-8")
+    )
+    is_active = True
+
+    @classmethod
+    async def build_async(cls, **kwargs: dict[str, Any]) -> User:
+        """Creates a User instance with dynamic keyword arguments."""
+        return cls._meta.model(**kwargs)
+
+
+class UserRegisterFactory(factory.Factory):
+    """Factory for creating user registration schemes."""
+
+    class Meta:
+        model = UserRegister
+
+    email = factory.Faker("email")
+    username = factory.Faker("user_name")
+    first_name = factory.Faker("first_name")
+    last_name = factory.Faker("last_name")
+    password = "testpassword"
+    confirm_password = "testpassword"
