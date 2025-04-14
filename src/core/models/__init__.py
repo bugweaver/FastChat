66c19b300968c4e0b4dc63fd3f4d528d343a0996
bugweaver <illustrati0@yandex.ru>
2025-04-14 09:07:14 +0300

    feat: add models to init file

--- src/core/models/__init__.py
+++ src/core/models/__init__.py
@@ -2,8 +2,16 @@ __all__ = (
     "db_helper",
     "Base",
     "User",
+    "Chat",
+    "ChatParticipant",
+    "Message",
+    "Attachment",
 )
 
+from .attachment_model import Attachment
 from .base import Base
+from .chat_model import Chat
+from .chat_part_model import ChatParticipant
 from .db_helper import db_helper
+from .message_model import Message
 from .user_model import User
